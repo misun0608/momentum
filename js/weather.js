56ab41c89e112221b5d372171c89f965971e6fbc
Misun Lim <chicmsl@gmail.com>
2023-03-05 18:48:33 +0900

    change css

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "b7ce8ffafb253c1ae94bb652cc607721";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log(`You live in ${lat} ${lon}`);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
